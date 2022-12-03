c18f7a569804fe5572db71c284c5319c436a0b5e
rksingh222 <62049275+rksingh222@users.noreply.github.com>
2022-12-03 12:30:29 +1100

    keys in terminal where npm start is pressed 3 Dec 2020

---
+++ npmstartterminalimportantkeys.js
@@ -0,0 +1,16 @@
+/**
+ *  Press a | Open android
+ *  Shift + a | Select a device or emulator
+ *  prss i | Open iOS Simultor
+ *  Shift + i | Select a simultor
+ *  press w | open web
+ *  press r | reload app
+ *  press m | toggle menu
+ *  shift + m | more tools
+ *  press j | open javascript inspector for Hermes
+ *  press o | open project code in your editor
+ *  press c | Show projet QR
+ *  press p | toggle build mode
+ *  press d | show developer tools
+ *  shift + d | toggle auto opening developer tools on startup
+ */
