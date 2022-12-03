3e3cd1434d61fb4a0f18a4e613a584e123975fdf
rksingh222 <62049275+rksingh222@users.noreply.github.com>
2022-12-03 12:42:42 +1100

    Update and rename npmstartterminalimportantkeys.js to npmstartterminalimportantkeysandreactdevtools.js

--- npmstartterminalimportantkeysandreactdevtools.js
+++ npmstartterminalimportantkeysandreactdevtools.js
@@ -14,3 +14,23 @@
  *  press d | show developer tools
  *  shift + d | toggle auto opening developer tools on startup
  */
+
+/**
+* in react native terminal visual studio
+* sudo npm install -g react-devtools
+* once its installed
+* 
+* in same terminal
+* type this command in terminal
+* react-devtools
+* it will open a window
+* in simulator 
+* press d for developer mode
+* enable Remote debug JS
+* it shows a tree
+* you click on component
+* you can see state event handler and you can change state
+* it is useful when 
+* to stop the debugging
+* press d
+* stop remote debugging
