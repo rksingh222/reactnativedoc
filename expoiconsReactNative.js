7503c40c9b94d9f47617f745308e56fadc66e974
rksingh222 <62049275+rksingh222@users.noreply.github.com>
2022-12-06 00:50:24 +1100

    expo icons react native 6 Dec 2022

---
+++ expoiconsReactNative.js
@@ -0,0 +1,21 @@
+/**
+ * 
+ * Expo icons applying in any area
+ * 
+ * Working with icons in Expo or Vector Icons you can learn from google
+ * 
+ * Expo comes with its default package icon without installing anything
+ * 
+ * we can use Iconicons
+ * 
+ * import { Ionicons } from '@expo/vector-icons 
+ * 
+ *  expo icons can be found here in google link
+ * 
+ * md-remove is one
+ * 
+ * <Ionicons name="md-remove" size={24} color="white or hexcode">
+ * 
+ * md-add for + inside primaryButton 
+ * 
+ */
