a4693417aabedd388ff6b3244b35e782669323a8
rksingh222 <62049275+rksingh222@users.noreply.github.com>
2022-12-06 00:20:46 +1100

    how to install react native through expo

---
+++ reactnativeexpoinstall.js
@@ -0,0 +1,56 @@
+/**
+ * sudo npm install -g expo-cli
+ * 
+ * expo init projectone
+ * 
+ * to run the app on the simulator
+ * 
+ * search for expo
+ * 
+ * Expo Go app
+ * 
+ * download in the device
+ * 
+ * opern in terminal
+ * 
+ * npm start
+ * 
+ * it also gives QR code to scan the qr code in Android 
+ * 
+ * scan QR code with android not on ios
+ * 
+ * in addition your browser will open with a bar code 
+ * 
+ * Now on the iphone the scanner is not integrated into the expo app
+ * 
+ * In terminal 
+ * 
+ * open -a simulator
+ * 
+ * expo client:install:ios
+ * for android
+ * 
+ * Click on configure
+ * 
+ * go to AVD manager pixel 3 best suited
+ * 
+ * click on finish
+ * 
+ * run to run the device
+ * 
+ * to open android emulator
+ * go to Android Studio
+ * 
+ * Click
+ * expo client:install:android
+ * 
+ * expo start 
+ * 
+ * 
+ * you will see a browser showing a qr code
+ * 
+ * Run on ios simulator
+ * 
+ * Run on android simulator
+ * 
+ */
