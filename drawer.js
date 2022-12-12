95b688edcf9d6b17387dab662c3897984bbc0c2a
rksingh222 <62049275+rksingh222@users.noreply.github.com>
2022-12-12 22:50:03 +1100

    Update drawer.js

--- drawer.js
+++ drawer.js
@@ -18,14 +18,32 @@
  * npm install react-native-reanimated@1 --save --save-exact
  */
 
+/*
+ * Drawer also opens navigation and route to that component
+ * navigation.toggleDrawer() --- to open the drawer if the drawer is closed and to close the drawer if the drawer is opened
+ */
+
+
 import  {NavigationContainer} from '@react-navigation/native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
+//this import is only for expo apps and remove drawerIcon if you are not using expo icon
+import { Ionicons } from '@exp/vector-icons' ;
 const Drawer = createDrawerNavigator();
 export default function App(){
     return (
         <NavigationContainer>
            <Drawer.Navigator initialRouteName='welcome'>
-               <Drawer.screen name='welcome' component={WelcomeScreen} />
+               <Drawer.screen name='welcome' component={WelcomeScreen} 
+                 options= {{
+                    headerStyle: {backgroundColor : '#3c06ab'},
+                    headerTintColor: 'white', /* header text color like the triple line and the text */
+                    drawerLabel: 'Welcome Screen', /* drawer triple line when selected it shows the screen name there */
+                    drawerActiveBackgroundColor : '#f0e1ff',
+                    drawerActiveTintColor : '#3c0a6b',
+                    drawerStyle: {backgroundColor: '#ccc'}, /* overall drawer bg color */
+                    drawerIcon:  ({color, size}) => (<Ionicons name="home" color={color} size={size}/>),
+                 }}
+               />
                <Drawer.screen name='user' component={UserScreen} />
            </Drawer.Navigator>
         </NavigationContainer>
