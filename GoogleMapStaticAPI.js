8451f1d727d1b666499da2287e5e554ee22560e2
rksingh222 <62049275+rksingh222@users.noreply.github.com>
2023-01-12 20:49:38 +1100

    Google Map Static API

---
+++ GoogleMapStaticAPI.js
@@ -0,0 +1,37 @@
+/**
+ * To access google maps in React Native 
+ * 
+ * go to google and search Maps Static API
+ * 
+ * you need a google account and credit card information
+ * 
+ * click near YOUR_API_KEY
+ * 
+ * click on Get Started
+ * 
+ * Google Cloud Platfor
+ * 
+ * Create a new fproject
+ * 
+ * go to APIs 
+ * 
+ * Maps Static API
+ * 
+ * go to credential and create credentical
+ * 
+ * create an api key
+ * 
+ * overview 
+ * the url and tweak it with API Key
+ * 
+ * we will get lattitude value and longitude value
+ * 
+ * https://maps.googleapis.com/maps/api/staticmap?center=${lat}
+ * change the size as you like 400*200
+ * markers color red
+ * label:s%2c${lat},${lng}
+ * key=${API_KEY}
+ * 
+ * this url needs to be send to the Image with uri: "url"
+ * to set the image 
+ */
