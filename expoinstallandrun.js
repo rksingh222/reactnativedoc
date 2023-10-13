3ada57a3a4362abf7fdd2a7e8e1a88e164253805
rksingh222 <62049275+rksingh222@users.noreply.github.com>
2023-10-13 22:45:50 +0530

    Update expoinstallandrun.js

--- expoinstallandrun.js
+++ expoinstallandrun.js
@@ -5,3 +5,15 @@ it will show the list of option to start on the simulator
 or on the real device by scanning the qr code
 to run the app on device
 install expo go 
+
+
+Drawer Issue
+-------------
+When creating a drawer Navigator you might across an error related to reanimated 
+after presets add plugins
+presets: ['babel-preset-expo'],
+    plugins: [
+      "react-native-reanimated/plugin",
+    ],
+and then save the app
+start the running process
